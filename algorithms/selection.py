b7649e13c964d683e2209d7960bc1c76896440b0
mateusz_stec <stec975@gmail.com>
2025-05-12 18:58:25 +0200

    fixed max function

--- algorithms/selection.py
+++ algorithms/selection.py
@@ -1,17 +1,23 @@
 
 import numpy as np
 
-def tournament_selection(population, fitness, tournament_size):
+def tournament_selection(population, fitness, tournament_size, optimization_goal="min"):
     fitness = np.array(fitness)
     new_population = []
     for _ in range(len(population)):
         tournament = np.random.choice(len(population), tournament_size, replace=False)
-        winner = tournament[np.argmin(fitness[tournament])]
+        if optimization_goal == "min":
+            winner = tournament[np.argmin(fitness[tournament])]
+        else:  
+            winner = tournament[np.argmax(fitness[tournament])]
         new_population.append(population[winner])
     return np.array(new_population)
 
-def roulette_selection(population, fitness):
-    fitness = [1/f for f in fitness] 
+def roulette_selection(population, fitness, optimization_goal="min"):
+    fitness = np.array(fitness)
+    if optimization_goal == "min":
+    
+        fitness = 1.0 / (fitness + 1e-10)
     fitness_sum = np.sum(fitness)
     probabilities = (fitness / fitness_sum) 
     selected_indices = np.random.choice(len(population), size=len(population), p=probabilities)
